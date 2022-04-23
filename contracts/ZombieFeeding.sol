829e38d8849a7fa2c5476b6c94ea5f857efc17c7
vineetaparodkar <>
2022-04-23 23:28:08 +0530

    completed zombies attack their victims challenge

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -1,5 +1,7 @@
 pragma solidity >=0.5.0 <0.6.0;
-import "./zombiefactory.sol";
+
+import "./ZombieFactory.sol";
+
 contract KittyInterface {
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
